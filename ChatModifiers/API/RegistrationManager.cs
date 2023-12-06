11a328654ee383af437a5df3c1f0e90ec2713633
Riley <90689870+speecil@users.noreply.github.com>
2023-12-06 11:07:53 +1100

    Rewrite registeration to give more info

--- ChatModifiers/API/RegistrationManager.cs
+++ ChatModifiers/API/RegistrationManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ChatModifiers.API
 {
@@ -6,15 +7,54 @@ namespace ChatModifiers.API
     {
         internal static List<CustomModifier> _registeredModifiers = new List<CustomModifier>();
 
-        public static void RegisterModifier(CustomModifier modifier)
+        public static bool RegisterModifier(CustomModifier modifier)
         {
-            _registeredModifiers.Add(modifier);
-            Plugin.Log.Info($"Registered Modifier: {modifier.Name}");
+            try
+            {
+                if (_registeredModifiers.Contains(modifier))
+                {
+                    Plugin.Log.Info($"Modifier {modifier.Name} is already registered.");
+                    return false;
+                }
+
+                _registeredModifiers.Add(modifier);
+                Plugin.Log.Info($"Registered Modifier: {modifier.Name}");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Plugin.Log.Error($"Error during registering modifier {modifier.Name}: {ex.Message}");
+                return false;
+            }
+            finally
+            {
+                Plugin.Log.Info($"Registration {(RegistrationManager._registeredModifiers.Contains(modifier) ? "successful" : "failed")} for modifier {modifier.Name}");
+            }
         }
-        public static void UnregisterModifier(CustomModifier modifier)
+
+        public static bool UnregisterModifier(CustomModifier modifier)
         {
-            _registeredModifiers.Remove(modifier);
-            Plugin.Log.Info($"Unregistered Modifier: {modifier.Name}");
+            try
+            {
+                if (!_registeredModifiers.Contains(modifier))
+                {
+                    Plugin.Log.Info($"Modifier {modifier.Name} is not registered.");
+                    return false;
+                }
+
+                _registeredModifiers.Remove(modifier);
+                Plugin.Log.Info($"Unregistered Modifier: {modifier.Name}");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Plugin.Log.Error($"Error during unregistering modifier {modifier.Name}: {ex.Message}");
+                return false;
+            }
+            finally
+            {
+                Plugin.Log.Info($"Unregistration {(RegistrationManager._registeredModifiers.Contains(modifier) ? "failed" : "successful")} for modifier {modifier.Name}");
+            }
         }
     }
 }
